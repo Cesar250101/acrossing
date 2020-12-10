1c401eb4080a4edb3789d61dabb34290d677101a
Your Name <you@example.com>
2020-12-10 12:30:48 -0300

    first commit

--- models/models.py
+++ models/models.py
@@ -5,10 +5,32 @@ from odoo import models, fields, api
 class Productos(models.Model):
     _inherit = 'product.template'
 
-    variable1 = fields.Char(
-        string='Variable n°1',
+    variable1= fields.Many2one(
+        comodel_name='acrossing.variable1',
+        string='Variable #1',
         required=False)
-    variable2 = fields.Char(
-        string='Variable n°2',
+    variable2 = fields.Many2one(
+        comodel_name='acrossing.variable2',
+        string='Variable #2',
         required=False)
+    marca = fields.Many2one(
+        comodel_name='acrossing.marca_producto',
+        string='Marca del Producto',
+        required=False)
+
+class Marcas(models.Model):
+    _name = 'acrossing.marca_producto'
+
+    name = fields.Char()
+
+class Marcas(models.Model):
+    _name = 'acrossing.variable1'
+
+    name = fields.Char()
+
+
+class Marcas(models.Model):
+    _name = 'acrossing.variable2'
+
+    name = fields.Char()
 
